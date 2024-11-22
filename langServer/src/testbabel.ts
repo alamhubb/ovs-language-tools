ec360225b1b0d10ed80535af5ab679ef46044f01
alamhubb <alamhubb@163.com>
2024-11-22 13:39:13 +0800

    treesitter

--- langServer/src/testbabel.ts
+++ langServer/src/testbabel.ts
@@ -1,26 +1,14 @@
-import * as ts from 'typescript';
-import JsonUtil from "./utils/JsonUtil.ts";
+import * as esprima from 'esprima';
 
-// 1. 解析代码到 AST
-const code = 'let a =';
-const sourceFile = ts.createSourceFile(
-    'example.ts',
-    code,
-    ts.ScriptTarget.Latest,
-    false
-);
+// 1. 基本解析
+const code = "let a = ";
 
-// 2. 查看 AST 结构
-JsonUtil.log(sourceFile.statements);
-
-// 3. 创建 printer
-const printer = ts.createPrinter({
-    newLine: ts.NewLineKind.LineFeed,
-    // 可选配置
-    removeComments: false,
-    omitTrailingSemicolon: false
+// 2. 使用 tolerant 选项进行容错解析
+const ast = esprima.parseScript(code, {
+    tolerant: true,      // 启用容错模式
+    tokens: true,        // 包含词法标记
+    range: true,         // 包含范围信息
+    comment: true,       // 包含注释
+    loc: true           // 包含位置信息
 });
 
-// 4. 生成代码
-const result = printer.printFile(sourceFile);
-console.log('Generated code:', result);
