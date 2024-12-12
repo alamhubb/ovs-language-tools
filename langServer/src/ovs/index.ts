5ba1eccb02cd497c0fbdf0f269033063d42a5c5d
alamhubb <alamhubb@163.com>
2024-12-12 11:15:51 +0800

    支持生成代码和sourcemap了

--- langServer/src/ovs/index.ts
+++ langServer/src/ovs/index.ts
@@ -4,15 +4,9 @@ import SubhutiLexer from '../../../subhuti/src/parser/SubhutiLexer.ts'
 import {es6Tokens} from 'subhuti-ts/src/language/es2015/Es6Tokens.ts'
 import SubhutiCst from "../../../subhuti/src/struct/SubhutiCst.ts";
 import JsonUtil from "../../../subhuti/src/utils/JsonUtil.ts";
-import OvsParser from "./parser/OvsParser.ts";
-import {ovsToAstUtil} from "./factory/Es6CstToOvsAstUtil.ts";
-import OvsAPI from "./OvsAPI.ts";
-import generate from "@babel/generator";
-import {LogUtil} from "../logutil.ts";
-import traverse from '@babel/traverse';
-import {MappingConverter} from "../languagePlugin.ts";
-import * as recast from "recast";
-
+import Es6Parser from "slime-parser/src/language/es2015/Es6Parser.ts";
+import SlimeCstToAstUtil from "slime-parser/src/language/SlimeLiteralAstUtil.ts";
+import SlimeGenerator from "slime-generator/src/SlimeGenerator";
 
 export function traverseClearTokens(currentNode: SubhutiCst) {
     if (!currentNode || !currentNode.children || !currentNode.children.length)
@@ -40,88 +34,26 @@ export function traverseClearLoc(currentNode: SubhutiCst) {
 }
 
 export function vitePluginOvsTransform(code) {
-    LogUtil.log('44444')
+
     const lexer = new SubhutiLexer(es6Tokens)
     const tokens = lexer.lexer(code)
-    JsonUtil.log(tokens)
     if (!tokens.length) return code
-    const parser = new OvsParser(tokens)
+    const parser = new Es6Parser(tokens)
 
-    let code1 = ''
+    let code1 = null
     let curCst = parser.Program()
-    // JsonUtil.log(7777)
     curCst = traverseClearTokens(curCst)
     // curCst = traverseClearLoc(curCst)
-    // JsonUtil.log(curCst)
-    // JsonUtil.log(curCst)
-    //cst转 estree ast
-    const ast = ovsToAstUtil.createFileAst(curCst)
-
+    // JsonUtil.log(7777)
+    // curCst = traverseClearTokens(curCst)
+    JsonUtil.log(curCst)
+    const ast = SlimeCstToAstUtil.toProgram(curCst)
     JsonUtil.log(ast)
-    let genRes = null
-    // 验证 AST 节点是否包含位置信息
-    /*traverse(ast, {
-        enter(path) {
-            path.node.start = undefined
-            path.node.end = undefined
-            if (!path.node.loc) {
-                throw Error(`Node of type ${path.node.type} is missing loc information.`);
-            }
-            path.node.loc.start.index = undefined
-            path.node.loc.end.index = undefined
-        }
-    });*/
-    try {
+    const code11 = SlimeGenerator.generator(ast)
+    console.log(code11.code)
+    console.log(code11.mappings)
 
-        // const output = recast.print(ast).code;
-        // LogUtil.log(output)
-        // // LogUtil.log(ast)
-        // LogUtil.log('6666')
-
-// 生成代码
-        genRes = generate(ast, {
-            sourceMaps: true,             // 启用源码映射
-            sourceFileName: 'source.js',  // 源文件名
-            fileName: 'generated.js',     // 生成文件名
-            retainLines: true,            // 保留行号
-            compact: false,               // 不压缩代码
-            comments: true,
-        });
-
-        code1 = genRes.code
-
-        if (code1) {
-            code1 = removeSemicolons(code1)
-        }
-    } catch (e) {
-        LogUtil.log('styleErrrrrrrr3333')
-        LogUtil.log(e.message)
-    }
-
-    function removeSemicolons(code) {
-        // 按行分割，处理每行，然后重新组合
-        return code.replace(/;$/gm, '')
-    }
-
-    //ast to client ast
-    // TokenProvider.visitNode(ast)
-    // JsonUtil.log(TokenProvider.tokens)
-    // OvsAPI.createVNode('div', 123)
-
-    // code1 = parser.exec()
-    // console.log(code1)
-    // const mapping = new OvsMappingParser()
-    // mapping.openMappingMode(curCst)
-    // code1 = mapping.exec(curCst)
-    // console.log(code1)
-    console.log(66666)
-    console.log(code1)
-    console.log(genRes)
-    console.log(genRes?.rawMappings)
-    return {
-        code: code1,
-        mapping: genRes?.rawMappings
-    }
+    return code11
     /*    return `
         // import OvsAPI from "@/ovs/OvsAPI.ts";\n
         ${code1.code}
@@ -130,14 +62,13 @@ export function vitePluginOvsTransform(code) {
 
 // const code = `let a = 'di
 const code = `let a = 1
-let b = 2
-let c = 3
-
-let div1 = function() {
-    return OvsAPI.createVNode("div", [a, b])
-}()
 `
-
+// let b = 2
+// let c = 3
+//
+// let div1 = function() {
+//     return OvsAPI.createVNode("div", [a, b])
+// }()
 // const code = `let a = div{
 //             header = div{123},
 //             true
