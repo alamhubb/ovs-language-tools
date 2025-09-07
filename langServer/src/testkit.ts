0e67e827948aa699cf6b11a8c7b34df4e3e0f6ca
alamhubb <alamhubb@163.com>
2025-09-07 13:19:37 +0800

    支持手动调用ts的提示功能

--- langServer/src/testkit.ts
+++ langServer/src/testkit.ts
@@ -1,11 +1,33 @@
-import kit from '@volar/kit';
+import * as kit from '@volar/kit';
 import {ovsLanguagePlugin} from "./languagePlugin";
 import {createTypeScriptServices} from "./typescript";
 import * as fs from "node:fs";
 import * as path from "node:path";
 import {loadTsdkByPath} from "@volar/language-server/node";
+import {createLanguage, createUriMap, ProjectContext} from "@volar/kit";
+import * as ts from "typescript";
+import {asFileName, asPosix, asUri} from "@volar/kit/lib/utils.ts";
+import {createLanguageServiceHost, resolveFileLanguageId, TypeScriptProjectHost} from "@volar/typescript";
+import {createServiceEnvironment} from "@volar/kit/lib/createServiceEnvironment.ts";
+import {arrayItemsEqual} from "@volar/typescript/lib/quickstart/createLanguageServicePlugin.ts";
+import {getUserPreferences} from "./typescript/lib/configs/getUserPreferences.ts";
+import type {SharedContext} from "./typescript/lib/semanticFeatures/types.ts";
+import {GetCompletionsAtPositionOptions} from "typescript";
+import {safeCall} from "./typescript/lib/shared.ts";
+import {Testkitlog} from "./testkitlog.ts";
 
-console.log(kit)
+
+const documentRegistries: [boolean, string, ts.DocumentRegistry][] = [];
+
+function getDocumentRegistry(ts: typeof import('typescript'), useCaseSensitiveFileNames: boolean, currentDirectory: string) {
+  let documentRegistry = documentRegistries.find(item => item[0] === useCaseSensitiveFileNames && item[1] === currentDirectory)?.[2];
+  if (!documentRegistry) {
+    documentRegistry = ts.createDocumentRegistry(useCaseSensitiveFileNames, currentDirectory);
+    documentRegistries.push([useCaseSensitiveFileNames, currentDirectory, documentRegistry]);
+  }
+  return documentRegistry;
+}
+
 
 const languagePlugins = [
   ovsLanguagePlugin
@@ -25,7 +47,7 @@ const services = [...createTypeScriptServices(tsdk.typescript)];
 
 function getTsconfig() {
 
-  let tsconfig = path.resolve(process.cwd(), './tsconfig.json');
+  let tsconfig = path.resolve(process.cwd(), '../tsconfig.json');
 
   const tsconfigIndex = process.argv.indexOf('--tsconfig');
   if (tsconfigIndex >= 0) {
@@ -39,6 +61,184 @@ function getTsconfig() {
   return tsconfig;
 }
 
-const tsconfigPath = getTsconfig()
+const tsconfig = getTsconfig()
 let includeProjectReference = false
-const linter = kit.createTypeScriptChecker(languagePlugins, services, tsconfigPath, includeProjectReference)
+const linter = kit.createTypeScriptChecker(languagePlugins, services, tsconfig, includeProjectReference)
+
+
+const tsconfigPath = asPosix(tsconfig);
+const configFileName = tsconfigPath;
+
+const fsFileSnapshots = createUriMap<[number | undefined, ts.IScriptSnapshot | undefined]>();
+
+const language = createLanguage(
+  [
+    ...languagePlugins,
+    {getLanguageId: uri => resolveFileLanguageId(uri.path)},
+  ],
+  createUriMap(ts.sys.useCaseSensitiveFileNames),
+  (uri, includeFsFiles) => {
+    if (!includeFsFiles) {
+      return;
+    }
+    const cache = fsFileSnapshots.get(uri);
+    const fileName = asFileName(uri);
+    const modifiedTime = ts.sys.getModifiedTime?.(fileName)?.valueOf();
+    if (!cache || cache[0] !== modifiedTime) {
+      if (ts.sys.fileExists(fileName)) {
+        const text = ts.sys.readFile(fileName);
+        const snapshot = text !== undefined ? ts.ScriptSnapshot.fromString(text) : undefined;
+        fsFileSnapshots.set(uri, [modifiedTime, snapshot]);
+      } else {
+        fsFileSnapshots.set(uri, [modifiedTime, undefined]);
+      }
+    }
+    const snapshot = fsFileSnapshots.get(uri)?.[1];
+    if (snapshot) {
+      language.scripts.set(uri, snapshot);
+    } else {
+      language.scripts.delete(uri);
+    }
+  }
+);
+let settings = {};
+const env = createServiceEnvironment(() => settings);
+const getCommandLine = () => {
+  return ts.parseJsonSourceFileConfigFileContent(
+    ts.readJsonConfigFile(tsconfigPath, ts.sys.readFile),
+    ts.sys,
+    path.dirname(tsconfigPath),
+    undefined,
+    tsconfigPath,
+    undefined,
+    languagePlugins.map(plugin => plugin.typescript?.extraFileExtensions ?? []).flat()
+  );
+}
+
+let commandLine = getCommandLine();
+let shouldCheckRootFiles = false;
+let projectVersion = 0;
+
+function checkRootFilesUpdate() {
+  if (!shouldCheckRootFiles) {
+    return;
+  }
+  shouldCheckRootFiles = false;
+
+  const newCommandLine = getCommandLine();
+  if (!arrayItemsEqual(newCommandLine.fileNames, commandLine.fileNames)) {
+    commandLine.fileNames = newCommandLine.fileNames;
+    projectVersion++;
+  }
+}
+
+const resolvedFileNameByCommandLine = new WeakMap<ts.ParsedCommandLine, string[]>();
+
+const projectHost: TypeScriptProjectHost = {
+  getCurrentDirectory: () => env.workspaceFolders.length
+    ? asFileName(env.workspaceFolders[0])
+    : process.cwd(),
+  getCompilationSettings: () => {
+    return commandLine.options;
+  },
+  getProjectReferences: () => {
+    return commandLine.projectReferences;
+  },
+  getProjectVersion: () => {
+    checkRootFilesUpdate();
+    return projectVersion.toString();
+  },
+  getScriptFileNames: () => {
+    checkRootFilesUpdate();
+    let fileNames = resolvedFileNameByCommandLine.get(commandLine);
+    if (!fileNames) {
+      fileNames = commandLine.fileNames.map(asPosix);
+      resolvedFileNameByCommandLine.set(commandLine, fileNames);
+    }
+    return fileNames;
+  },
+};
+
+const uriConverter = {
+  asFileName,
+  asUri,
+}
+
+const languageServiceHostAll = createLanguageServiceHost(
+  ts,
+  ts.sys,
+  language,
+  s => uriConverter.asUri(s),
+  projectHost
+)
+
+const sys = ts.sys
+
+const languageServiceHost = languageServiceHostAll.languageServiceHost
+
+
+const languageService = ts.createLanguageService(
+  languageServiceHost,
+  getDocumentRegistry(ts, sys.useCaseSensitiveFileNames, languageServiceHost.getCurrentDirectory())
+);
+
+// c:/Users/qinkaiyuan/IdeaProjects/testovsplg1/src/fasdf.ovs.ts1.ts
+// E:/qkyproject/openproject/volar.js/langServer/src/fasdf.ovs.ts1.ts
+// 133
+const opts: GetCompletionsAtPositionOptions = {
+  "quotePreference": "auto",
+  "importModuleSpecifierEnding": "minimal",
+  "jsxAttributeCompletionStyle": "auto",
+  "allowTextChangesInNewFiles": true,
+  "providePrefixAndSuffixTextForRename": true,
+  "allowRenameOfImportPath": true,
+  "includeAutomaticOptionalChainCompletions": true,
+  "provideRefactorNotApplicableReason": true,
+  "generateReturnInDocTemplate": true,
+  "includeCompletionsForImportStatements": true,
+  "includeCompletionsWithSnippetText": true,
+  "includeCompletionsWithClassMemberSnippets": true,
+  "includeCompletionsWithObjectLiteralMethodSnippets": true,
+  "useLabelDetailsInCompletionEntries": true,
+  "allowIncompleteCompletions": true,
+  "displayPartsForJSDoc": true,
+  "includeInlayParameterNameHintsWhenArgumentMatchesName": false,
+  "includeInlayFunctionParameterTypeHints": false,
+  "includeInlayVariableTypeHints": false,
+  "includeInlayVariableTypeHintsWhenTypeMatchesName": false,
+  "includeInlayPropertyDeclarationTypeHints": false,
+  "includeInlayFunctionLikeReturnTypeHints": false,
+  "includeInlayEnumMemberValueHints": false,
+  "includeCompletionsForModuleExports": true,
+  "includeCompletionsWithInsertText": true,
+  "includePackageJsonAutoImports": "auto",
+  "triggerKind": 1
+}
+const res = languageService.getCompletionsAtPosition('D:/project/qkyproject/test-volar/langServer/src/testkitovs.ovs.ts1.ts', 1, opts)
+
+const fileName = 'D:/project/qkyproject/test-volar/langServer/src/testkitovs.ovs.ts1.ts'
+const program = languageService.getProgram();
+
+
+const sourceFile = program?.getSourceFile(fileName);
+
+const tsToken: ts.CancellationToken = {
+  isCancellationRequested() {
+    return languageServiceHost.getCancellationToken?.().isCancellationRequested() ?? false;
+  },
+  throwIfCancellationRequested() {
+  },
+};
+const syntacticDiagnostics = program.getSyntacticDiagnostics(sourceFile, tsToken);
+const suggestionDiagnostics = languageService.getSuggestionDiagnostics(fileName);
+
+const info = safeCall(() => languageService.getCompletionsAtPosition(fileName, 14, opts));
+
+console.log(syntacticDiagnostics)
+console.log(suggestionDiagnostics)
+
+const ary = info.entries.map(item => item.name)
+for (const item of info.entries) {
+  Testkitlog.log(item.name)
+  Testkitlog.log(item.sortText)
+}
