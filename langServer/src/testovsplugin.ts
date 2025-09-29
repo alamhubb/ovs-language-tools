2a02ca8c407bf4607fa6f95965acff77bd293339
alamhubb <alamhubb@163.com>
2025-09-29 09:05:47 +0800

    支持ovs使用ts的提示

--- langServer/src/testovsplugin.ts
+++ langServer/src/testovsplugin.ts
@@ -4,6 +4,13 @@ const cases: Record<string, string> = {
   simpleCall: `console.log(1)`,
   ovsDivLiteral: `div {
   123
+}`,
+  ovsNested: `const a = 1
+div {
+  '123',
+  div {
+    a
+  }
 }`
 }
 
@@ -11,6 +18,5 @@ for (const [name, code] of Object.entries(cases)) {
   const res = vitePluginOvsTransform(code)
   console.log(`=== ${name} ===`)
   console.log('code:', res.code)
-  console.log('mapping:', res.mapping)
 }
 
